ed3c5ba150f704f444106186c83bec4d4f220868
Joao <joao.inacio.scrimini@gmail.com>
2022-05-25 20:38:04 -0300

    add base

---
+++ github.R
@@ -0,0 +1,9 @@
+#usethis::create_project("C:/Users/joaoi/Desktop/teste")
+usethis::use_git_config(user.name="Joao",user.email="joao.inacio.scrimini@gmail.com")
+usethis::use_git()
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+
+
+mtcars |>
+  View()
